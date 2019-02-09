0ebf2f95f592ff05e20731740cce5ee967dc5e85
kapive <kapive@ttu.ee>
2019-02-09 20:15:26 +0200

    final

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,15 @@
 package hello;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+    @CrossOrigin(origins = "http://localhost:8083")
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-
 }
